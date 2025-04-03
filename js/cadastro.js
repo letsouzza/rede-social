868213a37356acae380ea74a2686fb25334d205c
LETICIA SOUZA ALMEIDA <24122661@CFP127ALUNO.LOCAL>
2025-04-03 10:06:56 -0300

    Criando nova tela Perfil

--- js/cadastro.js
+++ js/cadastro.js
@@ -55,8 +55,27 @@ const cadastrarUser = async () => {
     }
 }
 
-const ocultarSenha = async () => {
-    
-}
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const senhaInput = document.getElementById("senha1");
+//     const confirmarSenhaInput = document.getElementById("senha2");
+//     const escondido = document.getElementById("senhaEscondida");
+
+//     const imgOlhoAberto = "https://cdn-icons-png.flaticon.com/512/11502/11502607.png";
+//     const imgOlhoFechado = "https://cdn-icons-png.flaticon.com/512/9726/9726597.png";
+
+//     escondido.addEventListener("click", () => {
+//         if (senhaInput.type === "password") {
+//             senhaInput.type = "text";
+//             confirmarSenhaInput = "text"   
+//             escondido.src = imgOlhoAberto; 
+//         } else {
+//             senhaInput.type = "password";
+//             confirmarSenhaInput.type = "password"
+//             escondido.src = imgOlhoFechado; 
+//         }
+//     });
+// });
+
 
 buttonCadastrar.addEventListener('click', cadastrarUser)
